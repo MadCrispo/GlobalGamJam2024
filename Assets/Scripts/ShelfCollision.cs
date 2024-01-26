bbec62ff1bc244e4a1bdb45c493f9cb73906017e
Cerz1 <alessiodiiorios5@gmail.com>
2024-01-26 18:04:03 +0100

    Revert "Particles_Prefab"
    This reverts commit 4df78649dec0c9b81346301bb899beb6a4590b2c.

--- Assets/Scripts/ShelfCollision.cs
+++ Assets/Scripts/ShelfCollision.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class ShelfCollision : MonoBehaviour
 {
-    public int numItem = 1;
-    public Products product = Products.bread;
+    // The cart TAG
+    private const string CartTag = "Cart";
 
+    public int numItem=1;
+    public Products product=Products.bread;
     // The particle system reference
     public ParticleSystem collisionParticles;
 
     private void Start()
     {
-        // Is the particle system assigned?
+       // Is the particle system assigned?
         if (collisionParticles == null)
         {
             Debug.LogError("Particle system not assigned. Please assign it in the Unity Editor.");
@@ -24,27 +26,30 @@ public class ShelfCollision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Check if the collided object has the specified tag
+        //if (collision.gameObject.CompareTag(CartTag))
+        //{
+            Debug.Log("Owner collided with a Cart!");
 
-        Debug.Log("Owner collided with a Cart!");
+            if (collisionParticles != null)
+            {
+                // The first Spoint of contact
+                ContactPoint contactPoint = collision.GetContact(0);
 
-        if (collisionParticles != null)
-        {
-            // The first Spoint of contact
-            ContactPoint contactPoint = collision.GetContact(0);
+                // Particles starts in the collision point
+                collisionParticles.transform.position = contactPoint.point;
 
-            // Particles starts in the collision point
-            collisionParticles.transform.position = contactPoint.point;
-
-            // Particles start
-            collisionParticles.Play();
-        }
+                // Particles start
+                collisionParticles.Play();
+            }
 
 
-        if (numItem > 0 && PlayerWallet.instance.CanIBuyIt(product))
-        {
-            numItem--;
-            PlayerWallet.instance.BuyStuff(product);
-            InventoryItems.AddItem(product);
-        }
+            if (numItem > 0 && PlayerWallet.instance.CanIBuyIt(product))
+            {
+                numItem--;
+                PlayerWallet.instance.BuyStuff(product);
+                InventoryItems.AddItem(product);
+            }
+        //}
     }
 }
